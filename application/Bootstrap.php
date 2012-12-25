742c02fe1e6b29bb6228bb46fb1e4f5c77bf65ee
mickaël andrieu <andrieu.travail@gmail.com>
2012-12-26 00:58:31 +0100

    adding Zf project

---
+++ application/Bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+
+    public function _initAutoloader()
+    {
+        $loader = Zend_Loader_Autoloader::getInstance();
+        $loader->setFallbackAutoloader(true);
+    }
+
+    protected function _initDatabase()
+    {
+        $adapter = Zend_Db::factory('pdo_mysql', array(
+            'host' => 'localhost',
+            'username' => 'amplurimedia',
+            'password' => 'A1nges6!',
+            'dbname' => 'pokemonmania',
+            'charset' => 'utf8'
+        ));
+        Zend_Db_Table_Abstract::setDefaultAdapter($adapter);
+    }
+}
+
---
+++ application/controllers/PokemonsController.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Mickaël Andrieu
+ * Date: 25/12/12
+ * Time: 22:47
+ * Pokemons 'CRUD' Controller : allow Create, Modify and Delete a Pokemon
+ */
+class PokemonsController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        $this->Pokemons = new Pokemons();
+    }
+
+
+    public function addAction()
+    {
+        $form = $this->getForm();
+        if($this->getRequest()->isPost())
+        {
+            $pokemonData = $this->getRequest()->getPost();
+            if($form->isValid($pokemonData))
+            {
+                $formData = $form->getValues();
+                $this->Pokemons->insert($formData);
+            }
+            else
+            {
+                $form->populate($pokemonData);
+            }
+        }
+        $this->view->form = $form;
+    }
+
+    public function indexAction()
+    {
+        $this->view->pokemons = $this->Pokemons->fetchAll();
+    }
+
+    public function showAction()
+    {
+        $id = $this->getRequest()->getParam('id');
+        if ($id > 0)
+        {
+            $pokemon = $this->Pokemons->find($id)->current();
+            $this->view->pokemon = $pokemon;
+        }
+        else $this->view->message = 'This Pokemon was not discovered yet !';
+    }
+    public function editAction()
+    {
+        $form = $this->getForm();
+        $id = $this->getRequest()->getParam('id');
+        if ($id > 0) {
+            if ($this->getRequest()->isPost())
+            {
+                $pokemonData = $this->getRequest()->getPost();
+                if ($form->isValid($pokemonData))
+                {
+                    $formData = $form->getValues();
+                    $this->Posts->update($formData, "id = $id");
+                    $this->_redirect('/pokemons/index');
+                }
+                else $form->populate($pokemonData);
+            }
+            else {
+                $pokemon = $this->Pokemons->find($id)->current();
+                $form->populate($pokemon->toArray());
+                $hidden = new Zend_Form_Element_Hidden('id');
+                $hidden->setValue($id);
+                $form->addElement($hidden);
+            }
+        }
+        else $this->view->message = 'You tryin\' to edit an undiscovered Pokemon!' ;
+        $this->view->form = $form;
+    }
+    public function delAction()
+    {
+        $id = $this->getRequest()->getParam('id');
+        if ($id > 0)
+        {
+            $pokemon = $this->Pokemons->find($id)->current();
+            $pokemon->delete();
+            $this->_redirect('/pokemons/index');
+        }
+    }
+
+    public function getForm()
+    {
+        $name = new Zend_Form_Element_Text('name');
+        $name->setLabel('Nom')
+            ->setDescription('Insérez le nom du Pokemon ici')
+            ->setRequired(true)
+            ->addValidator('StringLength', false, array(6, 20))
+            ->addFilters(array('StringTrim'));
+
+        $picture = new Zend_Form_Element_Text('picture');
+        $picture->setLabel('Url de l\'image')
+            ->setDescription('Insérez l\'url d\'accès à l\'image du Pokemon')
+            ->setRequired(true);
+            //->addFilters(array('StringToLower', 'StringTrim'));
+
+        $description = new Zend_Form_Element_Textarea('description');
+        $description->setLabel('Description')
+            ->setRequired(false)
+            ->setDescription('Description du pokemon');
+
+        $submit = new Zend_Form_Element_Submit('submit');
+        $submit->setLabel('Valider le formulaire')
+               ->setIgnore(true);
+
+        $form = new Zend_Form();
+        $form->addElements(array($name, $picture, $description, $submit));
+        // ->setAction('') // you can set your action. We will let blank, to send the request to the same action
+        return $form; // return the form
+    }
+}
+
